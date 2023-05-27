dbd2230882fa90321dc6178beafb5189fa6e6c23
edwinolivares24@gmail.com <edwinolivares24@gmail.com>
2023-05-26 22:27:42 -0500

    Done

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Write your solution in this file!
+function updateEmployeeWithKeyAndValue(Sam, key, value) {
+    return {...Sam, [key]: value};
+    //=> 
+}
+let employee = {
+    name: "Sam",
+    streetAddress: "123 Main St"
+  };
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+  }
+  function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = { ...employee };
+    delete newEmployee[key];
+    return newEmployee;
+  }
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+  }
